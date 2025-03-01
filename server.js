c89ecbaa577510377f7e4af880ef6e13c6edd777
Katt <kitcat0962@gmail.com>
2025-03-01 17:27:10 -0500

    listen on both 80 and 443 todo: 443 needs certificate information

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
+const path = require('path')
+const http = require('http'),
+    https = require('https')
 const express = require('express')
 const app = express()
-const path = require('path')
-
-const http = 80,
-    https = 443;
 
 app.use(express.static(path.join(__dirname, 'dist')))
-app.listen(http, console.log)
+
+// for testing purposes, use both http and https
+// when https testing is done (mainly need a certificate), remove the http
+http.createServer(app).listen(80);
+// certificate information goes here
+const https_credentials = {}
+https.createServer(https_credentials, app).listen(443);
+
