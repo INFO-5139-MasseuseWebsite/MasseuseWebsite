bafefe3797d13816dac44e9bc8c7d5cc32da2ce6
Katt <kitcat0962@gmail.com>
2025-03-01 20:34:55 -0500

    add api auth restrictions template

--- server.js
+++ server.js
@@ -5,7 +5,12 @@ const database = require('./server/database.js')
 const express = require('express')
 const app = express()
 
-app.post('/api/add-booking', (request, response) => {
+// Blanket auth for public
+app.post('/api/public/:handle', (request, response, next) => {
+    console.log('Accessing public...')
+    next()
+})
+app.post('/api/public/add-booking', (request, response) => {
     // verify request
 
     // if valid
@@ -17,6 +22,40 @@ app.post('/api/add-booking', (request, response) => {
     response.send()
     // else
     // send not valid response
+})
+
+// Blanket auth for RMTs
+app.post('/api/rmt/:handle', (request, response, next) => {
+    // authenticate. Replace with FireStore Admin
+    console.log('Accessing RMT...')
+    if (request.headers['x-auth']) {
+        console.log(`Authed as ${request.headers['x-auth']}`)
+        next()
+        return
+    }
+    console.log('Unauthorized')
+    response.status(401).send('401 Unauthorized')
+})
+app.post('/api/rmt/dothing', (request, response, next) => {
+    console.log('Accessing dothing...')
+    response.status(200).send()
+})
+
+// Blanket auth for Admins
+app.post('/api/admin/:handle', (request, response, next) => {
+    // authenticate. Replace with FireStore Admin
+    console.log('Accessing Admin...')
+    if (request.headers['x-auth']) {
+        console.log(`Authed as ${request.headers['x-auth']}`)
+        next()
+        return
+    }
+    console.log('Unauthorized')
+    response.status(401).send('401 Unauthorized')
+})
+app.post('/api/admin/dothing', (request, response, next) => {
+    console.log('Accessing dothing...')
+    response.status(200).send()
 })
 
 // This makes *everything* within the dist folder public.
