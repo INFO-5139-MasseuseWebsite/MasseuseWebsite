c731844a43da17949d2b6bc0bf1cb8845a2505f5
Katt <kitcat0962@gmail.com>
2025-03-04 21:45:12 -0500

    added float, vector_n, string_n. Redefined number to be a safe, non-infinite, non-NaN float

--- server/formParser.js
+++ server/formParser.js
@@ -1,17 +1,3 @@
-// Primitives
-export const NULL = v => [v == undefined, null]
-export const NUMBER = typeof 0
-export const INTEGER = v => [Number.isSafeInteger(v), v]
-export const STRING = typeof ''
-export const ARRAY = v => [Array.isArray(v), v]
-
-// Modifier Constructors
-export const NULLABLE = type => OR(NULL, type)
-export const ARRAY_T = type => AND(ARRAY, v => {
-    const vv = v.map(value => checkType(type, value)).filter(([valid, value]) => valid).map(([valid, value]) => value)
-    return [vv.length === v.length, vv]
-})
-
 // Union Constructors
 export const OR = (typeA, typeB) => v => {
     const [valid, value] = checkType(typeA, v)
@@ -23,6 +9,26 @@ export const AND = (typeA, typeB) => v => {
     if (!valid) return [valid, value]
     return checkType(typeB, value)
 }
+
+// Modifier Constructors
+export const NULLABLE = type => OR(NULL, type)
+export const STRING_N = length => AND(STRING, v => [v.length <= length, v])
+export const ARRAY_T = type => AND(ARRAY, v => {
+    const vv = v.map(value => checkType(type, value)).filter(([valid, value]) => valid).map(([valid, value]) => value)
+    return [vv.length === v.length, vv]
+})
+export const VECTOR_N = length => AND(ARRAY_T(NUMBER), v=>[v.length === length, v])
+
+// Primitives
+export const NULL = v => [v == undefined, null]
+export const BOOLEAN = typeof true
+export const FLOAT = typeof 0
+export const NUMBER = AND(FLOAT, v => [Number.isFinite(v), v])
+export const INTEGER = v => [Number.isSafeInteger(v), v]
+export const STRING = typeof ''
+export const ARRAY = v => [Array.isArray(v), v]
+export const VECTOR2 = VECTOR_N(2)
+export const VECTOR3 = VECTOR_N(3)
 
 // Custom Primitives
 const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
