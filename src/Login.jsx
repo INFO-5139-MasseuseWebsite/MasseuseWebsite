14ac6b6d91c5d7b9669ced3066c1e236ec0e556e
Minjae Kim <minjaekim9929@gmail.com>
2025-03-05 23:39:41 -0500

    Add login form and add CSS styling

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,9 +1,39 @@
 import React, { useState } from 'react';
+import './Login.css';
 
 const Login = () => {
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+
 	return (
-		<div className="login-page">
-			<h1>Login Page</h1>
+		<div className="login-container">
+			<h2>Login</h2>
+			<form>
+				<label htmlFor="username">Username</label>
+				<input
+					type="text"
+					placeholder="Type your username"
+					value={username}
+					id="username"
+					name="username"
+					required
+					onChange={(e) => setUsername(e.target.value)}
+					className="text-style"
+				/>
+
+				<label htmlFor="password">Password</label>
+				<input
+					type="password"
+					placeholder="Type your password"
+					value={password}
+					id="password"
+					name="password"
+					required
+					onChange={(e) => setPassword(e.target.value)}
+					className="text-style"
+				/>
+				<button type="submit">Login</button>
+			</form>
 		</div>
 	);
 };
