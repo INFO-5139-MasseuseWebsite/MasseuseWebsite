abf3e57f1f896eea1099e47c0fdf6f0e27fc87f7
AlicjaDev <a_blonski@fanshaweonline.ca>
2025-03-20 15:21:41 -0400

    change to x

--- src/Links.jsx
+++ src/Links.jsx
@@ -7,11 +7,9 @@ function Links() {
         <Link to="/">Home</Link>
         <br />
         <Link to="/health-history">Health History</Link>
-<<<<<<< HEAD
         <br />
         <Link to="/book-now">Book Now</Link> 
-=======
->>>>>>> 4d41476e2c513e66b6b94bfba2a1b7ca0c84fa36
+
       </nav>
     </header>
   );
